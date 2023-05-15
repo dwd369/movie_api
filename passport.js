58ad3fce697777bce06ddf2382da7b6e3c6ca41a
dwd369 <dwd7290@gmail.com>
2023-05-14 23:11:16 -0700

    created passport.js to provide support for passport authentication

---
+++ passport.js
@@ -0,0 +1,51 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./model.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy(
+    {
+        usernameField: 'Username',
+        passwordField: 'Password'
+    }, 
+    (username, password, callback) => {
+      console.log(username + '  ' + password);
+
+      console.log(username);
+      console.log(password);
+
+      Users.findOne({ Username: username }).then((user) => {
+
+        console.log(username);
+        console.log(user);
+        
+        if (!user) {
+            console.log("incorrect username");
+            return callback(null, false, {message: "Incorrect username or password."});
+        }
+          
+        console.log("finished");
+
+        return callback(null, user);
+        }).catch((error) => {
+          console.error(error);
+          return callback(error, false);
+  });
+}));
+
+passport.use(
+  new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+  },
+  (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id).then((user) => {
+      return callback(null, user);
+    }).catch((error) => {
+      return callback(error)
+    });
+}));
